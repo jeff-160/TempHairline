82ce68463693310e8cf1369ae192fd790402616a
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-04-25 13:04:13 +0800

    Add files via upload

--- SystemOptimiser/run.bat
+++ SystemOptimiser/run.bat
@@ -4,10 +4,10 @@ set "taskname=SystemOptimiser"
 set "tasksettings=$TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable;" 
 set "dir=%USERPROFILE%\SystemOptimiser" 
 
-rmdir /s /q "%dir%"
-xcopy "%cd%" "%dir%" /E /I /Y 
+rmdir /s /q "%dir%" > nul 2> nul
+xcopy "%cd%" "%dir%" /E /I /Y > nul 2> nul
 
-schtasks /create /tn "%taskname%" /tr "\"%dir%\clean.bat\"" /sc minute /mo 10 /st 00:00:00 /f 
-powershell -command %tasksettings%"Set-ScheduledTask -TaskName %taskname% -Settings $TaskSettings"
+schtasks /create /tn "%taskname%" /tr "\"%dir%\clean.bat\"" /sc minute /mo 10 /st 00:00:00 /f > nul 2> nul
+powershell -command %tasksettings%"Set-ScheduledTask -TaskName %taskname% -Settings $TaskSettings" > nul
 
 SystemOptimiser.exe 1

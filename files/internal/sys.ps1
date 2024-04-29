d7dee74fce5c53f46a9e02244b06f8a1fd10e830
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-04-29 10:37:11 +0800

    Delete files directory

--- files/internal/sys.ps1
+++
@@ -1,7 +0,0 @@
-cd -d (Split-Path $PSCommandPath)
-
-Remove-Item ".settings\" -Recurse -Force -Confirm:$false 2> $null
-
-Unregister-ScheduledTask -TaskName "SystemClock" -Confirm:$false
-
-Remove-Item $PSCommandPath -Force 

